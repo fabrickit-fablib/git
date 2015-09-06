712008a784f27018a2dede47c691a24b637f778e
Shunya Kitada <syun.kitada@gmail.com>
2015-09-06 19:33:12 +0900

    updated

--- __init__.py
+++ __init__.py
@@ -4,22 +4,25 @@ import os
 from fabkit import run, filer, Package, conf
 
 
-git_dir = os.path.join(conf.REMOTE_TMP_DIR, 'git')
+tmp_git_dir = os.path.join(conf.REMOTE_TMP_DIR, 'git')
 
 
 def setup():
     Package('git').install()
-    filer.mkdir(git_dir, mode='777')
+    filer.mkdir(tmp_git_dir, mode='777')
 
 
-def sync(git_url, name=None):
-    if not name:
-        name = git_url.split(' ')[0].rsplit('/', 1)[1]
-
-    clone_dir = os.path.join(git_dir, name)
-    if not filer.exists(clone_dir):
-        run('git clone {0} {1}'.format(git_url, clone_dir))
+def sync(git_url, name=None, git_dir=None):
+    if git_dir:
+        git_base_dir = git_dir.rsplit('/', 1)[0]
+        filer.mkdir(git_base_dir, mode='777')
     else:
-        run('cd {0} && git pull'.format(clone_dir))
+        name = git_url.split(' ')[0].rsplit('/', 1)[1]
+        git_dir = os.path.join(git_dir, name)
 
-    return clone_dir
+    if not filer.exists(git_dir):
+        run('git clone {0} {1}'.format(git_url, git_dir))
+    else:
+        run('cd {0} && git pull'.format(git_dir))
+
+    return git_dir
