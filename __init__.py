93bae6923359815f9473fcda5391f7aaf1c65819
Shunya Kitada <syun.kitada@gmail.com>
2016-03-19 23:46:57 +0900

    update

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 import os
-from fabkit import run, sudo, filer, Package, expect
+from fabkit import run, sudo, filer, Package, expect, api
 from oslo_config import cfg
 
 CONF = cfg.CONF
@@ -22,11 +22,12 @@ def sync(url, branch='master', dest=None, user=None):
         dest = os.path.join(tmp_dest, name)
 
     if not filer.exists(dest):
-        expect('git clone -b {0} {1} {2}'.format(branch, url, dest),
-               expects=[
-               ['Are you sure you want to continue connecting (yes/no)?', 'yes\\n'],
-               ],
-               user=user)
+        with api.cd('/tmp'):
+            expect('git clone -b {0} {1} {2}'.format(branch, url, dest),
+                   expects=[
+                   ['Are you sure you want to continue connecting (yes/no)?', 'yes\\n'],
+                   ],
+                   user=user)
     else:
         if user is not None:
             sudo('cd {0} && git pull'.format(dest), user=user)
